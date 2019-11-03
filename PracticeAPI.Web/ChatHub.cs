631ba7a1783e26f32898a8400326d55434f54484
Stephen Hila <stephen.hila@gmail.com>
2019-11-03 22:19:40 +0800

    - add loading of previous messages - code cleanup

--- PracticeAPI.Web/ChatHub.cs
+++ PracticeAPI.Web/ChatHub.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 using System.Configuration;
 using System.Net.Http;
 using System.Net.Http.Headers;
@@ -21,11 +23,35 @@ namespace PracticeAPI.Web
 
                 string name = HttpContext.Current.User.Identity.Name;
 
-                HttpResponseMessage Res = client.PostAsJsonAsync<MessageModel>("api/Messages/Post", new MessageModel { Text = message, Username = name }).Result; // TODO: make this async
+                HttpResponseMessage Res = client.PostAsJsonAsync<MessageModel>("api/Messages/Post", new MessageModel { Text = message, Username = name }).Result;
 
                 if (Res.IsSuccessStatusCode)
                 {
                     Clients.All.addNewMessageToPage(name, message);
+                }
+            }
+        }
+
+        public void Load()
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(ConfigurationManager.AppSettings["apiUrl"]);
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                int messageCount = int.Parse(ConfigurationManager.AppSettings["maxChatMessages"]);
+
+                HttpResponseMessage Res = client.GetAsync($"api/Messages/Get?count={messageCount}").Result;
+
+                if (Res.IsSuccessStatusCode)
+                {
+                    var messages = Res.Content.ReadAsAsync<IEnumerable<MessageModel>>().Result;
+
+                    foreach (var message in messages)
+                    {
+                        Clients.Caller.addNewMessageToPage(message.Username, message.Text);
+                    }
                 }
             }
         }
